b81f8b1609747f1a7ebea520d7a72c6762086644
CHIK SUM <chiksum.wong@gmail.com>
2018-09-12 15:01:30 +0800

    Update Sources Code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+
+
+let store = createStore(todoApp)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+
+    document.getElementById('root')
+);
+
+
 registerServiceWorker();
